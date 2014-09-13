7cf6ee813393778d2cc936d5b180356b54a2ef54
MAxim <maximbmstu@gmail.com>
2014-09-13 14:59:06 +0400

    clear template

--- bitrix/templates/index/header.php
+++ bitrix/templates/index/header.php
@@ -8,9 +8,6 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/css/reset.css" class="css-finalize-read">
-		<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/css/fonts/stylesheet.css" class="css-finalize-read">
-		<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/css/style.css" class="css-finalize-read"><style class="css-finalized">.replacement span{transition:opacity 400ms linear;}form.success div.success{transition:opacity 400ms;}input, textarea{transition:all 300ms;}nav ul li a{transition:all 300ms ease;}/*************Main**************/.main{transition:all 600ms ease;}#main-about div.social ul li a{transition:all 300ms ease;}#main-work .works li a{transition:all 300ms ease;}#main-work .works li a img{transition:all 300ms ease;}#main-work .works li a span{transition:all 300ms ease;}#main-work .works li a span h2{transition:bottom 300ms ease;}#main-work .works li a span h4{transition:300ms bottom ease;}#main-work .works li a:hover h4{transition:300ms bottom 100ms ease;}#main-project #nav-projects a.back{transition:all 300ms;}#main-project #nav-projects ul li a{transition:background 300ms ease;}</style>
-
 		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.min.js"></script>
 		<script src="<?=SITE_TEMPLATE_PATH?>/js/knockout-2.0.0.js"></script>
 		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.address-1.4.min.js"></script>
@@ -19,15 +16,15 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		<script src="<?=SITE_TEMPLATE_PATH?>/js/actions.js"></script>
 
 		<!--[if lt IE 9]>
-			<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+			<script src="<?=SITE_TEMPLATE_PATH?>/js/IE9.js"></script>
 		<![endif]-->
 
 		<?$APPLICATION->ShowHead();?>
 
 		<title><?$APPLICATION->ShowTitle();?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+		<!-- <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> -->
 	</head>
 	<body>
-		<!-- <div id="panel"> -->
-			<?//$APPLICATION->ShowPanel();?>
-		<!-- </div> -->
+		<div id="panel">
+			<?$APPLICATION->ShowPanel();?>
+		</div>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(isset($_REQUEST['contact']))
 	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
 		if(empty($_REQUEST['nome']) && empty($_REQUEST['email']) && empty($_REQUEST['message']))
-			echo '{"error":true}';
+					echo json_encode(array('error' => true)); // echo '{"error":true}';
 		else
 		{
 			foreach ($_REQUEST as $key => $value) {
@@ -22,60 +22,147 @@ else
 	require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 	$APPLICATION->SetTitle('Главная');
 	?>
-
 	<div id="container">
 		<section class="main active" id="main-home" style="height: 0px;">
-			<nav>
-				<ul>
-					<li><a href="#/about" class="active">Обо мне</a></li>
-					<li><a href="#/work">Портфолио</a></li>
-					<li><a href="#/contact">Контакты</a></li>
-				</ul>
-			</nav>
-
-			<h1>Привет!</h1>
-			<h2>Меня зовут Максим Подвишенский и я создаю качественные сайты.</h2>
-			<small>Я разработчик <a href="http://1c-bitrix.ru">1c-Bitrix</a>.</small>
+			<?$APPLICATION->IncludeComponent("bitrix:menu","mp_menu_top",array(
+				"ROOT_MENU_TYPE" => "top",
+				"MAX_LEVEL" => "1",
+				"CHILD_MENU_TYPE" => "top",
+				"USE_EXT" => "N",
+				"DELAY" => "N",
+				"ALLOW_MULTI_SELECT" => "N",
+				"MENU_CACHE_TYPE" => "N",
+				"MENU_CACHE_TIME" => "3600",
+				"MENU_CACHE_USE_GROUPS" => "Y",
+				"MENU_CACHE_GET_VARS" => ""
+				)
+			);?>
+			<h1>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/main_page/h1.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h1>
+			<h2>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/main_page/h2.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h2>
+			<small>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/main_page/small.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</small>
 		</section>
 		<section class="main" id="main-about" style="height: auto;">
-			<h1>Итак...</h1>
-			<h2>Кто я такой и с чем меня едят.</h2>
+			<h1>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/h1.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h1>
+			<h2>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/h2.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h2>
 			<article class="box-really-like">
-				<h1>Я люблю</h1>
+				<h1>
+					<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/article_like_h1.php",
+						"EDIT_TEMPLATE" => ""
+						),
+						false
+					);?>
+				</h1>
 				<ul>
-					<li>WeB 	</li>
-					<li>Linux	</li>
-					<li>Apple 	</li>
-					<li>Bitrix 	</li>
-					<li>Кофе	</li>
-					<li>Музыку	</li>
+					<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/article_like_ul.php",
+						"EDIT_TEMPLATE" => ""
+						),
+						false
+					);?>
 				</ul>
 			</article>
 			<article class="box-bit-of-me">
-				<h1>Немного о себе</h1>
-				<p>Я Максим, мне 25 лет. Живу и работаю в Москве. Выпускник МГТУ им. Н.Э. Баумана по специальности "Комплексное обеспечение информационной безопасности автоматизированных систем".</p>
-				<p>У меня есть три года опыта разработки интернет-проектов. В общем, я действительно люблю свою работу. Мои навыки включают разработку сайтов на платформе 1c-Bitrix и полный пакет серверных технологий.</p>
-				<p>Самое главное, что нужно знать обо мне, что я создаю качественные сайты. Ныряю с головой в поставленные задачи и предлагаю оптимальные решения.</p>
-				<!-- <p></p> -->
-
-				<!-- <p>Мне 25 лет Я живу в Москве. Я закончил МГТУ им. Н.Э. Баумана по специальности "Информационная безопасность". У меня есть три года опыта работы с инернет проектами, и я действительно люблю свою работу. Короче, это я.</p> -->
-				<!-- <p>Самое главное, что нужно знать обо мне, что я люблю создавать качетвенные сайты.</p> -->
-				<!-- <p>Мои навыки включают разработку сайтов на платформе 1c-Bitrix и полный пакет серверных технологий.</p> -->
+				<h1>
+					<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/article_me_h1.php",
+						"EDIT_TEMPLATE" => ""
+						),
+						false
+					);?>
+				</h1>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/article_me.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
 			</article>
 			<div class="clearfix social">
 				<ul>
-					<li><a href="http://vk.com/id2332212"><i class="dribbble replacement">vk</i></a></li>
-					<li><a href="http://www.facebook.com/mp.gladiator"><i class="facebook replacement">facebook</i></a></li>
-					<li><a href="http://ru.linkedin.com/pub/максим-подвишенский/60/945/738/"><i class="linkedin replacement">linkedin</i></a></li>
-					<li><a href="http://brainstorage.me/MegBegb/portfolio"><i class="behance replacement">brainstorage</i></a></li>
+					<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => SITE_TEMPLATE_PATH . "/include/about_page/social.php",
+						"EDIT_TEMPLATE" => ""
+						),
+						false
+					);?>
 				</ul>
 			</div>
-			<address>© 2014 All rights reserved.</address>
+			<address>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/address.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</address>
 		</section>
 		<!-- <section class="main" id="main-work" data-projects="[{&quot;ordem&quot;:1,&quot;name&quot;:&quot;Unificado&quot;,&quot;type&quot;:&quot;Game Platform&quot;,&quot;uri&quot;:&quot;unificado&quot;,&quot;images&quot;:[&quot;img/project-unificado/img-01.png&quot;,&quot;img/project-unificado/img-02.png&quot;,&quot;img/project-unificado/img-03.png&quot;,&quot;img/project-unificado/img-04.png&quot;,&quot;img/project-unificado/img-05.png&quot;,&quot;img/project-unificado/img-06.png&quot;,&quot;img/project-unificado/img-07.png&quot;,&quot;img/project-unificado/img-08.png&quot;],&quot;bgcolor&quot;:&quot;#ffffff&quot;,&quot;paddingtop&quot;:&quot;364px&quot;},{&quot;ordem&quot;:2,&quot;name&quot;:&quot;Paquetá&quot;,&quot;type&quot;:&quot;Corporate website&quot;,&quot;uri&quot;:&quot;paqueta&quot;,&quot;images&quot;:[&quot;img/project-paqueta/img-01.jpg&quot;,&quot;img/project-paqueta/img-02.png&quot;,&quot;img/project-paqueta/img-03.jpg&quot;,&quot;img/project-paqueta/img-04.jpg&quot;,&quot;img/project-paqueta/img-05.jpg&quot;,&quot;img/project-paqueta/img-06.jpg&quot;],&quot;bgcolor&quot;:&quot;#54656d&quot;,&quot;paddingtop&quot;:&quot;0px&quot;},{&quot;ordem&quot;:3,&quot;name&quot;:&quot;Ponto Pronto&quot;,&quot;type&quot;:&quot;Corporate website&quot;,&quot;uri&quot;:&quot;ponto-pronto&quot;,&quot;images&quot;:[&quot;img/project-ponto-pronto/img-01.jpg&quot;,&quot;img/project-ponto-pronto/img-02.jpg&quot;,&quot;img/project-ponto-pronto/img-03.jpg&quot;,&quot;img/project-ponto-pronto/img-04.jpg&quot;],&quot;bgcolor&quot;:&quot;#1f1f1f&quot;,&quot;paddingtop&quot;:&quot;0px&quot;},{&quot;ordem&quot;:4,&quot;name&quot;:&quot;Sinoscar&quot;,&quot;type&quot;:&quot;Corporate website&quot;,&quot;uri&quot;:&quot;sinoscar&quot;,&quot;images&quot;:[&quot;img/project-sinoscar/img-01.jpg&quot;,&quot;img/project-sinoscar/img-02.png&quot;,&quot;img/project-sinoscar/img-03.jpg&quot;,&quot;img/project-sinoscar/img-04.jpg&quot;,&quot;img/project-sinoscar/img-05.jpg&quot;,&quot;img/project-sinoscar/img-06.jpg&quot;,&quot;img/project-sinoscar/img-07.jpg&quot;],&quot;bgcolor&quot;:&quot;#ffffff&quot;,&quot;paddingtop&quot;:&quot;0px&quot;},{&quot;ordem&quot;:5,&quot;name&quot;:&quot;Banrisul&quot;,&quot;type&quot;:&quot;Proposal&quot;,&quot;uri&quot;:&quot;banrisul&quot;,&quot;images&quot;:[&quot;img/project-banrisul/img-01.png&quot;],&quot;bgcolor&quot;:&quot;#0b68b3&quot;,&quot;paddingtop&quot;:&quot;112px&quot;},{&quot;ordem&quot;:6,&quot;name&quot;:&quot;Cambionet&quot;,&quot;type&quot;:&quot;Cambio exchange&quot;,&quot;uri&quot;:&quot;cambionet&quot;,&quot;images&quot;:[&quot;img/project-cambionet/img-01.png&quot;,&quot;img/project-cambionet/img-02.jpg&quot;,&quot;img/project-cambionet/img-03.jpg&quot;,&quot;img/project-cambionet/img-04.jpg&quot;,&quot;img/project-cambionet/img-05.jpg&quot;,&quot;img/project-cambionet/img-06.jpg&quot;,&quot;img/project-cambionet/img-07.png&quot;],&quot;bgcolor&quot;:&quot;#eeeeee&quot;,&quot;paddingtop&quot;:&quot;325px&quot;},{&quot;ordem&quot;:7,&quot;name&quot;:&quot;Común Tierra Project&quot;,&quot;type&quot;:&quot;Sustainable communities&quot;,&quot;uri&quot;:&quot;comun-tierra&quot;,&quot;images&quot;:[&quot;img/project-comun-tierra/img-01.png&quot;,&quot;img/project-comun-tierra/img-02.png&quot;,&quot;img/project-comun-tierra/img-03.png&quot;],&quot;bgcolor&quot;:&quot;#629b32&quot;,&quot;paddingtop&quot;:&quot;417px&quot;},{&quot;ordem&quot;:9,&quot;name&quot;:&quot;Residencial Pinheiro Grosso&quot;,&quot;type&quot;:&quot;Website&quot;,&quot;uri&quot;:&quot;pinheiro-grosso&quot;,&quot;images&quot;:[&quot;img/project-pinheiro-grosso/img-01.png&quot;,&quot;img/project-pinheiro-grosso/img-02.png&quot;],&quot;bgcolor&quot;:&quot;#bdbca6&quot;,&quot;paddingtop&quot;:&quot;90px&quot;},{&quot;ordem&quot;:9,&quot;name&quot;:&quot;Shopping Total&quot;,&quot;type&quot;:&quot;Website&quot;,&quot;uri&quot;:&quot;shopping-total&quot;,&quot;images&quot;:[&quot;img/project-shopping-total/img-01.png&quot;,&quot;img/project-shopping-total/img-02.jpg&quot;,&quot;img/project-shopping-total/img-03.jpg&quot;],&quot;bgcolor&quot;:&quot;#ece8e3&quot;,&quot;paddingtop&quot;:&quot;70px&quot;},{&quot;ordem&quot;:10,&quot;name&quot;:&quot;Foernges&quot;,&quot;type&quot;:&quot;Corporate website&quot;,&quot;uri&quot;:&quot;foernges&quot;,&quot;images&quot;:[&quot;img/project-foernges/img-01.png&quot;,&quot;img/project-foernges/img-02.jpg&quot;,&quot;img/project-foernges/img-03.jpg&quot;,&quot;img/project-foernges/img-04.jpg&quot;],&quot;bgcolor&quot;:&quot;#eeeeee&quot;,&quot;paddingtop&quot;:&quot;124px&quot;},{&quot;ordem&quot;:11,&quot;name&quot;:&quot;Senac-RS&quot;,&quot;type&quot;:&quot;Corporate website&quot;,&quot;uri&quot;:&quot;senac-rs&quot;,&quot;images&quot;:[&quot;img/project-senac-rs/img-01.png&quot;],&quot;bgcolor&quot;:&quot;#235f9e&quot;,&quot;paddingtop&quot;:&quot;100px&quot;},{&quot;ordem&quot;:12,&quot;name&quot;:&quot;Tennis Ranking&quot;,&quot;type&quot;:&quot;Associação Leopoldina Juvenil&quot;,&quot;uri&quot;:&quot;tennis-ranking&quot;,&quot;images&quot;:[&quot;img/project-tennis-ranking/img-01.jpg&quot;],&quot;bgcolor&quot;:&quot;#8d3e0b&quot;,&quot;paddingtop&quot;:&quot;197px&quot;}]" style="height: 0px;"> -->
 		<section class="main" id="main-work" data-projects="[{&quot;ordem&quot;:1,&quot;name&quot;:&quot;a1park&quot;,&quot;type&quot;:&quot;Сетьпарковок&quot;,&quot;uri&quot;:&quot;a1park&quot;,&quot;images&quot;:[&quot;img/project-a1park/a1park2.png&quot;],&quot;bgcolor&quot;:&quot;#ffffff&quot;,&quot;paddingtop&quot;:&quot;64px&quot;},{&quot;ordem&quot;:2,&quot;name&quot;:&quot;viosnet&quot;,&quot;type&quot;:&quot;Социальнаясеть&quot;,&quot;uri&quot;:&quot;viosnet&quot;,&quot;images&quot;:[&quot;img/project-viosnet/viosnet.png&quot;],&quot;bgcolor&quot;:&quot;#ffffff&quot;,&quot;paddingtop&quot;:&quot;64px&quot;},{&quot;ordem&quot;:3,&quot;name&quot;:&quot;mebelvia&quot;,&quot;type&quot;:&quot;Интернетмагазин&quot;,&quot;uri&quot;:&quot;mebelvia&quot;,&quot;images&quot;:[&quot;img/project-mebelvia/mebelvia.png&quot;],&quot;bgcolor&quot;:&quot;#ffffff&quot;,&quot;paddingtop&quot;:&quot;64px&quot;}]" style="height: 0px;">
-			 <h1>Проекты :)</h1>
-			<h2>Здесь находятся мои самые удачные работы.</h2>
+			<h1>
+			 	<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/work_page/h1.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h1>
+			<h2>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/work_page/h2.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h2>
 			 <script id="projects-list-tmpl" type="text/html">
 				<li class="loading">
 					<a data-bind="attr: {href: '#/project/'+uri}">
@@ -89,16 +176,53 @@ else
 				</li>
 			</script>
 			<ul class="clearfix works" data-bind="template: {name: 'projects-list-tmpl', foreach: projects, afterRender: loadImages}"></ul>
-			<small>Внимание! Все проекты, над которыми я работал, находятся тут -> <a href="http://brainstorage.me/MegBegb/portfolio">Brainstorage</a>.</small>
-			<address>© 2014 All rights reserved.</address>
+			<small>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/work_page/small.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</small>
+			<address>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/address.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</address>
 		</section>
 		<section class="main" id="main-contact" style="height: 0px;">
-
-			<h1>Пишите!</h1>
-			<h2>Свяжитесь со мной по любому вопросу.</h2>
-			<!-- <h2>If you're a real human being, feel free to send me an email.</h2> -->
-			<small>В настоящее время я доступен для любых проектов.</small>
-
+			<h1>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/contact_page/h1.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h1>
+			<h2>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/contact_page/h2.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</h2>
+			<small>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/contact_page/small.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</small>
 			<form action="?contact" method="post" class="ajax">
 				<fieldset>
 					<input type="text" name="nome" placeholder="Имя" class="required">
@@ -116,7 +240,15 @@ else
 				</div>
 			</form>
 
-			<address>© 2014 All rights reserved.</address>
+			<address>
+				<?$APPLICATION->IncludeComponent("bitrix:main.include", ".default", array(
+					"AREA_FILE_SHOW" => "file",
+					"PATH" => SITE_TEMPLATE_PATH . "/include/address.php",
+					"EDIT_TEMPLATE" => ""
+					),
+					false
+				);?>
+			</address>
 		</section>
 
 	</div>
